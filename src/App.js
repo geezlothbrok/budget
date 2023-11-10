9d22e4c24c9e1622e2171c2dcbde05241620342c
gideon ohene nsowah <giddyohene5@gmail.com>
2023-11-10 03:04:26 -1200

    nav bar integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import  Dashboard  from "../src/pages/Dashboard";
+import NavBar from "./components/navigation/NavBar";
+
 function App() {
   return (
     <div className="App">
-        <p>
-          geez lothbrok
-        </p>
+       <NavBar/>
+        <Routes>
+        <Route path="/" element = { <Dashboard/>}/>
+        </Routes>
+       
     </div>
   );
 }
--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./NavBar.css";
+import { FaGoogleWallet } from "react-icons/fa";
+import { BiUserCircle } from "react-icons/bi";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav>
+      <div className="navbar-container">
+        <div className="nav-logo-container">
+          <p className="logo-text">
+            
+            <span className="logo-icon">
+              <FaGoogleWallet />
+            </span>
+            Budget.io
+          </p>
+        </div>
+
+        <ul className="user-profile">
+          <li className="user-image">
+            <BiUserCircle />
+          </li>
+          <li className="user-name">
+            <h3>hi, user</h3>
+          </li>
+        </ul>
+
+        <ul className="mobile-links">
+          <li>settings</li>
+          <li>profile</li>
+          <li>transactions</li>
+          <li>add new</li>
+        </ul>
+
+        <ul className="toogle-mobile">
+          <li>
+            <button className="open-menu">
+              <HiMenuAlt4 />
+            </button>
+          </li>
+          <li>
+            <button className="close-menu">
+              <IoClose />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
