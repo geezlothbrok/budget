50c4c6e8b5e5f5be213b4f927130d8d04ecb6d1e
gideon ohene nsowah <giddyohene5@gmail.com>
2023-11-10 00:47:58 -1200

    begining of a new budget app

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAm7C9KzLQMBHNNakwW2KVJysHAlm-OkEs",
+  authDomain: "budget-app-a6d0e.firebaseapp.com",
+  projectId: "budget-app-a6d0e",
+  storageBucket: "budget-app-a6d0e.appspot.com",
+  messagingSenderId: "70904696615",
+  appId: "1:70904696615:web:292a3bb5e80c35e984a302"
+};
+
+const app = initializeApp(firebaseConfig);
