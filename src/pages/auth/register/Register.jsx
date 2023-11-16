8bb6179815ea692993ad463aeb9fd084809a52e7
gideon ohene nsowah <giddyohene5@gmail.com>
2023-11-16 04:05:16 -1200

    sign up interface construction

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -1,11 +1,55 @@
 import React from 'react';
 import "./Register.css";
+import register from "../../images/register.jpg";
+
+import { TfiEmail } from "react-icons/tfi";
+import { PiLockKeyOpen } from "react-icons/pi";
+import { FcGoogle } from "react-icons/fc";
+
+import { Link } from 'react-router-dom';
+
 
 function Register() {
   return (
     <div className="main-container">
-      <section className="image-container"></section>
-      <section className="form-container"></section>
+      <section className="image-container">
+        <img src= {register} alt="Sign up" />
+      </section>
+
+      <section className="form-container">
+        <p className="tittle">sign up</p>
+        <div className="form">
+          <span className="email-input">
+          <TfiEmail /> 
+          <input type="email" />
+          </span>
+        </div>
+
+        <div className="password">
+        <span className="email-input">
+          <PiLockKeyOpen /> 
+          <input type="password" />
+          </span>
+        <span className="email-input">
+          <PiLockKeyOpen /> 
+          <input type="password" />
+          </span>
+        </div>
+        <section className="actions">
+        <button type="submit" className='button'>Sign Up</button>
+        <p className="already-account">
+          I already have an account? <Link to = "/login">Sign In</Link>
+        </p>
+      </section>
+      <span className="left-line"></span>
+        or
+        <span className="right-line"></span>
+
+        <div className="google-button">
+          <button type="submit"><span>
+            <FcGoogle /></span>Google</button>
+        </div>
+      </section>     
     </div>
   )
 }
