863a59592fdd60c7c7fc355f82e7dbc469f29fc9
gideon ohene nsowah <giddyohene5@gmail.com>
2023-12-08 02:22:32 -1200

    add transaction form

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
         <Route path="/login" element = { <Login/>}/>
         <Route path="/register" element = { <Register/>}/>
         <Route path="/reset" element = { <Reset/>}/>
+        
         </Routes>
        </BrowserRouter>
     </div>
--- src/pages/AddNew.jsx
+++ src/pages/AddNew.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function AddNew() {
   return (
-    <div>AddNew</div>
+    <div>
+      <form action="" className="expense-form">
+        <h1 className="title">add transaction</h1>
+
+        <label htmlFor="" className="description" id="">Item</label>
+        <input type="text" placeholder='description' required />
+
+        <label htmlFor="" className="description" id="">Amount</label>
+        <input type="number" placeholder='Amount' required />
+
+        <label htmlFor="expense" className="description" >Expense</label>
+        <input type="radio" required id="expense" value="expense"/>
+
+        <label htmlFor="income" className="description" >Income</label>
+        <input type="radio" required id="income" value="income" />
+        <button type="submit">add</button>
+      </form>
+    </div>
   )
 }
 
