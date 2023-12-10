018c32fb7326cc63c51ce5198b5183d9097fdf45
gideon ohene nsowah <giddyohene5@gmail.com>
2023-12-10 02:44:04 -1200

    completed add transactions form interface for large screens

--- src/pages/AddNew.jsx
+++ src/pages/AddNew.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./AddNew.css";
 
 function AddNew() {
   return (
-    <div>
+    <div className="main-container">
       <form action="" className="expense-form">
         <h1 className="title">add transaction</h1>
+        <section className="input-section">
+          <label htmlFor="" className="description" id="">
+            Item
+          </label>
+          <input type="text" placeholder="description" required/>
 
-        <label htmlFor="" className="description" id="">Item</label>
-        <input type="text" placeholder='description' required />
+          <label htmlFor="" className="description" id="">
+            Amount
+          </label>
+          <input type="number" placeholder="Amount" required className="input"/>
+        </section>
 
-        <label htmlFor="" className="description" id="">Amount</label>
-        <input type="number" placeholder='Amount' required />
+        <section className="radio-section">
+          <div>
+            <label htmlFor="expense" className="description">
+            Expense
+          </label>
+          <input type="radio" required id="expense" value="expense" />
+          </div>
+          
+          <div>
+            <label htmlFor="income" className="description">
+            Income
+          </label>
+          <input type="radio" required id="income" value="income" />
+          </div>
+          
+        </section>
 
-        <label htmlFor="expense" className="description" >Expense</label>
-        <input type="radio" required id="expense" value="expense"/>
-
-        <label htmlFor="income" className="description" >Income</label>
-        <input type="radio" required id="income" value="income" />
         <button type="submit">add</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default AddNew
+export default AddNew;
